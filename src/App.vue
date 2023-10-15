3f4cd25c048067edce210edd452f23bf14f82e7d
sheldon arthur <sheldonarthursagrado@gmail.com>
2023-10-15 17:18:54 +0800

    changed the paths

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
 </template>
 
 <script>
-import Navbar from "./components/Navbar.vue";
-import Footer from "./components/Footer.vue";
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "App",
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -12,12 +12,12 @@
 </template>
 
 <script>
-import Cover from "../components/home/Cover.vue";
-import Overview from "../components/home/Overview.vue";
-import Features from "../components/home/Features.vue";
-import Services from "../components/home/Services.vue";
-import Hit from "../components/home/Hit.vue";
-import Map from "../components/home/Map.vue";
+import Cover from "@/components/home/Cover.vue";
+import Overview from "@/components/home/Overview.vue";
+import Features from "@/components/home/Features.vue";
+import Services from "@/components/home/Services.vue";
+import Hit from "@/components/home/Hit.vue";
+import Map from "@/components/home/Map.vue";
 
 export default {
   name: "LandingPage",
